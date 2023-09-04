8f39644093bd9c7d9c3625bf9f23f3fe06afe904
Hafsa1000 <143885492+Hafsa1000@users.noreply.github.com>
2023-09-04 23:19:51 +0530

    Create form.sql

---
+++ form.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(
+ uid INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL UNIQUE,
+ user_type ENUM('User', 'Engineer', 'Admin') NOT NULL, 
+ password VARCHAR(255) NOT NULL
+);
